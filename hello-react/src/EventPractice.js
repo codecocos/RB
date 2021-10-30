c44febcf8f1d0fe920d05902c56387786c8fdd1c
codecoco <88923210+codecocos@users.noreply.github.com>
2021-10-30 23:31:05 +0900

    211030_onChage onClick에 전달할 함수 따로 뺴내어 컴포넌트 임의 메서드 만듦

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -5,6 +5,29 @@ class EventPractice extends Component {
     message: ''
   }
 
+  constructor(props) {
+    super(props);
+    //함수가 호출될 때 this는 호출부에 따라 결정
+    //this를 컴포넌트 자신으로 제대로 가르키기 위해서는
+    //메서드를 this와 바인딩하는 작업이 필요
+    //만약 바인딩하지 않는 경우라면 this는 undefined를 나타냄.
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
+
   render() {
     return (
       <div>
@@ -14,22 +37,9 @@ class EventPractice extends Component {
           name='message'
           placeholder='아무거나 입력해 보세요'
           value={this.state.message}
-          onChange={
-            (e) => {
-              this.setState({
-                message: e.target.value
-              })
-            }
-          }
+          onChange={this.handleChange}
         />
-        <button onClick={
-          () => {
-            alert(this.state.message);
-            this.setState({
-              message: ''
-            });
-          }
-        }>확인</button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
