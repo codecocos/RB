bdfde10562a6bf60776997075ec6e88b4ae99362
codecoco <88923210+codecocos@users.noreply.github.com>
2021-10-31 11:20:51 +0900

    211031_컴포넌트에 ref 달기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import ScrollBox from './ScrollBox'
 import ValidationSample from './ValidationSample'
 
 class App extends Component {
   render() {
     return (
-      <ValidationSample />
+      <>
+        <ValidationSample />
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button>
+      </>
     )
   }
 }
