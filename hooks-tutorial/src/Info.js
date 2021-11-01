ca158de91f9845a34e7f0372558ef05ee115363b
codecoco <88923210+codecocos@users.noreply.github.com>
2021-11-01 16:04:53 +0900

    211101_useEffect 사용해보기

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    console.log(name);
+    //컴포넌트가 언마운트 되기 전이나, 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면 뒷정리 함수 반환
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+    //빈 배열을 넣어 맨 처음 렌더링 될 때만 실행 되게함.
+    //파라미터 제공 시 해당 파라미터가 업데이트 될 때만 실행됨.
+  }, [name]);
 
   const onChangeName = e => {
     setName(e.target.value);
