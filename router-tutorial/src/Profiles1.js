ce812c2daf0543696c472f2ad38d128798ff5b3b
codecoco <88923210+codecocos@users.noreply.github.com>
2021-11-03 00:20:36 +0900

    211103_NavLink 사용

--- router-tutorial/src/Profiles1.js
+++ router-tutorial/src/Profiles1.js
@@ -1,22 +1,18 @@
 
-import { NavLink, Route } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
+//import WithRouterSample from './WithRouterSample'
 import Profile from './Profile'
 
 const Profiles = () => {
-  const activeStyle = {
-    background: 'black',
-    color: 'white'
-  };
-
   return (
     <div>
       <h3>사용자 목록:</h3>
       <ul>
         <li>
-          <NavLink activeStyle={activeStyle} to='/profiles/velopert'>velopert</NavLink>
+          <Link to='/profiles/velopert'>velopert</Link>
         </li>
         <li>
-          <NavLink activeStyle={activeStyle} to='/profiles/gildong'>gildong</NavLink>
+          <Link to='/profiles/gildong'>gildong</Link>
         </li>
       </ul>
       <Route path='/profiles' exact render={() => <div>사용자를 선택해 주세요.</div>} />
