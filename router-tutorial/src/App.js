3c19947e482626e29935a93effd165103f8280de
codecoco <88923210+codecocos@users.noreply.github.com>
2021-11-02 21:54:03 +0900

    211102_ url 파라미터

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import { Link, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 
 
@@ -18,11 +19,21 @@ const App = () => {
         <li>
           <Link to='/info'>소개2</Link>
         </li>
+        <li>
+          <Link to='/profile/velopert'>velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to='/profile/gildong'>gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path='/' component={Home} exact={true} />
+
       {/* 라우터를 두 번 사용하는 대신, path props를 배열로 설정하면 여러 경로에서 같은 컴포넌트를 보여줄 수 있음. */}
       <Route path={['/about', '/info']} component={About} />
+
+      {/* match.params.username 값을 통해 현재 username 값을 조회 할 수 있음. */}
+      <Route path={'/profile/:username'} component={Profile} />
 
     </div>
   )
